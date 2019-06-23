c7cdeff37c3a628db7721ca61e8ecbd0ad6ddd15
Ochanissi <mirelalex2094@gmail.com>
2019-06-23 22:45:10 +0300

    completed another challenge

--- script.js
+++ script.js
@@ -1780,3 +1780,43 @@ function shellSort(arr) {
 console.log(shellSort([3, 0, 2, 5, -1, 4, 1]));
 
 console.timeEnd('shellSort');
+
+
+
+// ***************************************************************************
+console.log('**************************************************************');
+// ***************************************************************************
+
+/////////////////////////////////
+// CODING CHALLENGE 21
+
+// Write a JavaScript program to sort a list of elements using Bubble sort.
+
+/////////////////////////////////
+
+console.time('swap');
+
+function swap(arr, first_Index, second_Index){
+    var temp = arr[first_Index];
+    arr[first_Index] = arr[second_Index];
+    arr[second_Index] = temp;
+}
+
+function bubble_Sort(arr){
+
+    var len = arr.length,
+        i, j, stop;
+
+    for (i=0; i < len; i++){
+        for (j=0, stop=len-i; j < stop; j++){
+            if (arr[j] > arr[j+1]){
+                swap(arr, j, j+1);
+            }
+        }
+    }
+
+    return arr;
+}
+console.log(bubble_Sort([3, 0, 2, 5, -1, 4, 1]));
+
+console.timeEnd('swap');
