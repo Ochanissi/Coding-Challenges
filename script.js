341d157886d54b61df263b0e67c7cb15f7c2f3ae
Ochanissi <mirelalex2094@gmail.com>
2019-07-16 16:10:56 +0300

    completed another algorithm

--- script.js
+++ script.js
@@ -2067,3 +2067,51 @@ console.log("Sorted Array Elements");
 console.log(flash_sort(arra, 0, 5));
 
 console.timeEnd('flash_sort');
+
+
+// ***************************************************************************
+console.log('**************************************************************');
+// ***************************************************************************
+
+/////////////////////////////////
+// CODING CHALLENGE 25
+
+// Write a JavaScript program to sort a list of elements using Pancake sort.
+
+/////////////////////////////////
+
+console.time('pancake_sort');
+
+function pancake_sort(arr) {
+    for (var i = arr.length - 1; i >= 1; i--) {
+        // find the index of the largest element not yet sorted
+        var max_idx = 0;
+        var max = arr[0];
+        for (var j = 1; j <= i; j++) {
+            if (arr[j] > max) {
+                max = arr[j];
+                max_idx = j;
+            }
+        }
+ 
+        if (max_idx == i) 
+            continue; // element already in place
+ 
+        var new_slice;
+ 
+        // flip arr max element to index 0
+        if (max_idx > 0) {
+            new_slice = arr.slice(0, max_idx+1).reverse();
+            for ( j = 0; j <= max_idx; j++) 
+                arr[j] = new_slice[j];
+        }
+ 
+        // then flip the max element to its place
+        new_slice = arr.slice(0, i+1).reverse();
+        for ( j = 0; j <= i; j++) 
+            arr[j] = new_slice[j];
+    }
+    return arr;
+}
+
+console.timeEnd('pancake_sort');
