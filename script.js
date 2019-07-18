426752c7d32e889982e8905e5458a12d415d5917
Ochanissi <mirelalex2094@gmail.com>
2019-07-18 16:38:29 +0300

    completed another algorithm

--- script.js
+++ script.js
@@ -2115,3 +2115,62 @@ function pancake_sort(arr) {
 }
 
 console.timeEnd('pancake_sort');
+
+// ***************************************************************************
+console.log('**************************************************************');
+// ***************************************************************************
+
+/////////////////////////////////
+// CODING CHALLENGE 26
+
+// Write a JavaScript program to sort a list of elements using Bogosort.
+
+/////////////////////////////////
+
+console.time('Bogosort');
+
+function Bogosort(arr){
+    var isSorted = function(arr){
+        for(var i = 1; i < arr.length; i++){
+            if (arr[i-1] > arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    function shuffle(arr){
+        var count = arr.length, temp, index;
+
+        while(count > 0){
+            index = Math.floor(Math.random() * count);
+            count--;
+
+            temp = arr[count];
+            arr[count] = arr[index];
+            arr[index] = temp;
+        }
+
+        return arr;
+    }
+
+   function sort(arr){
+        var sorted = false;
+        while(!sorted){
+            arr = shuffle(arr);
+            sorted = isSorted(arr);
+        }
+        return arr;
+    }
+
+    return sort(arr);
+}
+
+
+var arra = [3, 0, 2, 5, -1, 4, 1]; 
+console.log("Original Array Elements"); 
+console.log(arra); 
+console.log("Sorted Array Elements"); 
+console.log(Bogosort(arra));
+
+console.time('Bogosort');
