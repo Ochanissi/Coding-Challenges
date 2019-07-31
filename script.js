9b18e39132a12e701604a600ec38937cee92ece5
Ochanissi <mirelalex2094@gmail.com>
2019-07-31 18:04:24 +0300

    completed another challenge

--- script.js
+++ script.js
@@ -2561,3 +2561,48 @@ console.log(num_string_range(0, 25, 5));
 console.log(num_string_range(20, 5, 5));
 
 console.timeEnd('Coding Challenge 36');
+
+
+// ***************************************************************************
+console.log('**************************************************************');
+// ***************************************************************************
+
+/////////////////////////////////
+// CODING CHALLENGE 37
+
+// Write a JavaScript program to sort the items of an array.
+
+/////////////////////////////////
+
+console.time('Coding Challenge 37');
+
+var arr1=[-3,8,7,6,5,-4,3,2,1];
+var arr2=[];
+var min=arr1[0];
+var pos;
+var max=arr1[0];
+for (i=0; i<arr1.length; i++)
+{
+        if (max<arr1[i]) max=arr1[i];
+}
+
+for (var i=0;i<arr1.length;i++)
+{
+        for (var j=0;j<arr1.length;j++)
+        {
+                if (arr1[j]!="x")
+                {
+                        if (min>arr1[j]) 
+                        {
+                                min=arr1[j];
+                                pos=j;
+                        }
+                }
+        }
+        arr2[i]=min;
+        arr1[pos]="x";
+        min=max;
+}
+console.log(arr2);
+
+console.timeEnd('Coding Challenge 37');
