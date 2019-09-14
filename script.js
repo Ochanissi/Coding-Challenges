cc111b400982989878354789e3120b385fe0b70f
Ochanissi <mirelalex2094@gmail.com>
2019-09-14 16:43:21 +0300

    completed another challenge

--- script.js
+++ script.js
@@ -2927,3 +2927,37 @@ function power_of_2(n) {
    console.log(power_of_2(16));
    console.log(power_of_2(18));
    console.log(power_of_2(256));
+
+   // ***************************************************************************
+console.log('**************************************************************');
+// ***************************************************************************
+
+/////////////////////////////////
+// CODING CHALLENGE 47
+
+// Write a JavaScript function to convert a decimal number to binary, hexadecimal or octal number.
+
+dec_to_bho  = function(n, base) {
+ 
+    if (n < 0) {
+      n = 0xFFFFFFFF + n + 1;
+     } 
+switch (base)  
+{  
+case 'B':  
+return parseInt(n, 10).toString(2);
+break;  
+case 'H':  
+return parseInt(n, 10).toString(16);
+break;  
+case 'O':  
+return parseInt(n, 10).toString(8);
+break;  
+default:  
+return("Wrong input.........");  
+}  
+}
+
+console.log(dec_to_bho(120,'B'));
+console.log(dec_to_bho(120,'H'));
+console.log(dec_to_bho(120,'O'));
