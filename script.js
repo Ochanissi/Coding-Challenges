1426cb7305d6256174fbd663020d8dee3ea28b70
Ochanissi <mirelalex2094@gmail.com>
2019-09-16 15:56:40 +0300

    completed another small problem

--- script.js
+++ script.js
@@ -2981,3 +2981,32 @@ function max(input) {
 
 console.log(max([12,34,56,1]));
 console.log(max([-12,-34,0,-56,-1]));
+
+
+// ***************************************************************************
+console.log('**************************************************************');
+// ***************************************************************************
+
+/////////////////////////////////
+// CODING CHALLENGE 49
+
+// Write a JavaScript function to calculate the sum of values in an array.
+
+function sum(input){
+             
+    if (toString.call(input) !== "[object Array]")
+       return false;
+         
+               var total =  0;
+               for(var i=0;i<input.length;i++)
+                 {                  
+                   if(isNaN(input[i])){
+                   continue;
+                    }
+                     total += Number(input[i]);
+                  }
+                return total;
+               }
+   console.log(sum([1,2,3]));
+   console.log(sum([100,-200,3]));
+   console.log(sum([1,2,'a',3]));
