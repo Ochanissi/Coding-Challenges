14bc9a149d9fe10359021e3e0c39d22640d7315f
Mirel Home <mirelalex2094@gmail.com>
2020-11-13 22:17:07 +0200

    completed another challenge

--- script.js
+++ script.js
@@ -29243,3 +29243,26 @@ isBoiling("104C");
 isBoiling("-10F");
 
 
+/////////////////////////////////
+// CODING CHALLENGE 646
+
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+function cubes(a) {
+	return a ** 3;
+}
+
+
+cubes=a=>a**3
+
+
+function cubes(a) {
+	return a * a * a;
+}
+
+
+cubes(2);
+cubes(3);
+cubes(4);
+cubes(5);
+cubes(10);
