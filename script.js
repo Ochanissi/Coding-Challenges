034bbee9a62c35bf0482a4a792c83cf3ec3a215a
Mirel Home <mirelalex2094@gmail.com>
2021-01-27 00:12:46 +0200

    completed another challenge

--- script.js
+++ script.js
@@ -31267,3 +31267,29 @@ shortestDistance('354972,19774,976623,664356');
 shortestDistance('587568,609626,654834,18784');
 shortestDistance('66662,790481,873587,545905');
 shortestDistance('332500,825806,905569,98242');
+
+
+/////////////////////////////////
+// CODING CHALLENGE 700
+
+// Write a function that removes any non-letters from a string, returning a well-known film title.
+
+
+function lettersOnly(str) {
+	return str.replace(/[^A-Za-z]/g, "");  
+}
+
+function lettersOnly(str) {
+	return str.replace(/[^a-z]/gi, '');
+}
+
+lettersOnly(',1|2)")A^1<[_)?^"]l[a`3+%!d@8-0_0d.*}i@&n?=');
+lettersOnly('^U)6$22>8p).');
+lettersOnly('I5n!449+c@e*}@@1]p{2@`,~t:i0o%n<3%8');
+lettersOnly('!)"P[s9)"69}yc3+?1]+33>3ho');
+lettersOnly(':~;G{o}o{+524<df~:>}e24{l8:_l]a:?@]%s7');
+lettersOnly('&&S~]@8>1-0!h#r),80<_>!}|e>_k:');
+lettersOnly(')^/|,!!09]=/1<G2?`=[l{a}d9]^i7a0|t6_o2*r');
+lettersOnly(']8;]V9e{=0r!.5t>i<^_g"4o"5~');
+lettersOnly('%%)?"?B#>/_4a#,;t8|m8675a(n');
+lettersOnly('97H^)~a8567ll*o?"6%)w63e37e<n?@=');
