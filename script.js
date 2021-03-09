aac297930216d66e72932ee918e700ecd74843a8
Mirel Home <mirelalex2094@gmail.com>
2021-03-09 10:08:09 +0200

    completed another challenge

--- script.js
+++ script.js
@@ -33228,3 +33228,56 @@ sumOfSlices([58,3,	38,	99,	10]);
 sumOfSlices([13]);
 sumOfSlices([62,	406,	343,	234,	244,	200,	304,	462,	212,	48,	166,	71,	80,	51,	307,	442,	368,	235,	199,	411,	100,	203,	330,	437,	226,	365,	337,	464,	14,	350]);
 sumOfSlices([315,	47]);
+
+/////////////////////////////////
+// CODING CHALLENGE 743
+
+// Create the instance properties fullname and email in the Employee class. Given a person's first and last names:
+
+// Form the fullname by simply joining the first and last name together, separated by a space.
+// Form the email by joining the first and last name together with a . in between, and follow it with @company.com at the end. Make sure everything is in lowercase.
+
+
+class Employee {
+	constructor (firstname, lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.fullname = this.firstname + ' ' + this.lastname;
+		this.email = `${this.firstname}.${this.lastname}@company.com`.toLowerCase();
+	}
+}
+
+
+class Employee {
+	constructor (firstname, lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		// Complete the code!
+		this.fullname = `${firstname} ${lastname}`;
+		this.email = `${firstname}.${lastname}@company.com`.toLowerCase();
+	}
+}
+
+
+class Employee {
+	constructor (firstname, lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.fullname = `${firstname} ${lastname}`;
+		this.email = `${firstname}.${lastname}@company.com`.toLowerCase();
+	}
+}
+
+emp1 = new Employee('John', 'Smith');
+emp2 = new Employee('Mary',  'Sue');
+emp3 = new Employee('Antony', 'Walker');
+emp4 = new Employee('Joshua', 'Senoron');
+
+emp3.firstname;
+emp3.lastname;
+emp3.fullname;
+emp3.email;
+emp4.firstname;
+emp4.lastname;
+emp4.fullname;
+emp4.email;
