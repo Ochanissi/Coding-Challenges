2e6d3aa3dd24906994973bc9afc4e0c1ec193b3a
Mirel Home <mirelalex2094@gmail.com>
2021-03-28 17:52:51 +0300

    completed another challenge

--- script.js
+++ script.js
@@ -34188,3 +34188,40 @@ rightRotations("abc");
 rightRotations("abcdef");
 rightRotations("himalaya");
 rightRotations("aab");
+
+
+/////////////////////////////////
+// CODING CHALLENGE 763
+
+// The .length property on an array will return the number of elements in the array. For example, the array below contains 2 elements:
+
+// [1, [2, 3]]
+// // 2 elements, number 1 and array [2, 3]
+// Suppose we instead wanted to know the total number of non-nested items in the nested array. In the above case, [1, [2, 3]] contains 3 non-nested items, 1, 2 and 3.
+
+// Write a function that returns the total number of non-nested items in a nested array.
+
+
+function getLength(arr) {
+	return arr.flat(Infinity).length;
+}
+
+
+const flatten = arr =>
+  arr.reduce((result, inner) => {
+    if (Array.isArray(inner)) return result.concat(flatten(inner));
+    return result.concat(inner);
+  }, []);
+
+const getLength = arr => flatten(arr).length;
+
+
+getLength([1, [2,3]]);
+getLength([1, [2, [3, 4]]]);
+getLength([1, [2, [3, [4, [5, 6]]]]]);
+getLength([1, 7, 8]);
+getLength([2]);
+getLength([2, [3], 4, [7]]);
+getLength([2, [3, [5, 7]], 4, [7]]);
+getLength([2, [3, [4, [5]]], [9]]);
+getLength([]);
